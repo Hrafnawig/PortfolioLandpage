0702b6c9dff97493b0390e2a7e8a5226d873d90e
Hrafnawig <kobhraf@gmail.com>
2022-03-22 17:42:48 +0200

    UI start

--- landpage/src/pages/HomeComponents/Title.jsx
+++ landpage/src/pages/HomeComponents/Title.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import logo from '../../media/download 1.svg'
+import logo from '../../media/download 1.png'
+import search from '../../media/bi_search.png'
 import styles from './styles.module.scss';
 const Title = () => {
     return (
-        <div className= {styles.title}>
-         <img src={logo}/>
+        <div>
+            <div className={styles.center}><img className={styles.logo} src={logo}/></div>
+            <div className={styles.center}>
+                <div className={styles.center}>
+                    <div className={styles.space}>Our Hotel</div>
+                    <div className={styles.space}>Room & Rates</div>
+                    <div className={styles.space}>Facilities</div>
+                    <div className={styles.space}>Contact Us</div>
+                    <div className={styles.space}><img src={search}/></div>
+                </div>
+            </div>
         </div>
     );
 };
--- landpage/src/pages/HomePage.jsx
+++ landpage/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Title from './HomeComponents/Title';
+import OurHotel from './HomeComponents/OurHotel';
 const HomePage = () => {
     return (
         <div>
             <Title/>
+            <OurHotel/>
         </div>
     );
 };
