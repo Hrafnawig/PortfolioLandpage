d397c532e8cbb4d347fd7a0503236cb136597955
Hrafnawig <kobhraf@gmail.com>
2022-03-23 19:52:52 +0200

    static UI 1 part

--- landpage/src/pages/HomeComponents/Apartaments.jsx
+++ landpage/src/pages/HomeComponents/Apartaments.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import styles from './styles.module.scss';
 const Rooms = () => {
     return (
-        <div className={styles.center}>
+        <div className={styles.centerColumn}>
          <div className={styles.roomsTxt}>Rooms & Rates</div>
+            <div className={styles.rooms}>
+                <div className={`${styles.twinRoom} ${styles.centerColumn}`}>
+                    <div className={styles.roomdesc}>Standard Twin Room</div>
+                    <div>Check Rates</div>
+                </div>
+                <div className={`${styles.standartRoom} ${styles.centerColumn}`}>
+                    <div className={styles.roomdesc}>Standard Room</div>
+                    <div>Check Rates</div>
+                </div>
+                <div className={`${styles.viewRoom} ${styles.centerColumn}`}>
+                    <div className={styles.roomdesc}>Standard View Room</div>
+                    <div>Check Rates</div>
+                </div>
+                <div className={`${styles.deluxeRoom} ${styles.centerColumn}`}>
+                    <div className={styles.roomdesc}>Deluxe Room</div>
+                    <div>Check Rates</div>
+                </div>
+            </div>
         </div>
     );
 };
