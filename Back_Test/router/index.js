ac85be1e3e5748ab5f1fcbfec4ed01db341709af
Hrafnawig <kobhraf@gmail.com>
2022-03-30 19:26:56 +0300

    new back

---
+++ Back_Test/router/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const heroController= require('../controllers/superhero.controller');
+
+
+module.exports = router;
---
+++ Back_Test/seeders/20220330162517-seed-hotels.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Hotels', [
+      {city: 'ForestStadt', type: 'Standard twin', price: 300, createdAt:(new Date()), updatedAt:(new Date())},
+      {city: 'ForestStadt', type: 'Standard', price: 200, createdAt:(new Date()), updatedAt:(new Date())},
+      {city: 'ForestStadt', type: 'Deluxe', price: 400, createdAt:(new Date()), updatedAt:(new Date())},
+      {city: 'ForestStadt', type: 'Standard view', price: 300, createdAt:(new Date()), updatedAt:(new Date())},
+      {city: 'DesertStan', type: 'Standard twin', price: 200, createdAt:(new Date()), updatedAt:(new Date()) },
+      {city: 'DesertStan', type: 'Standard', price: 100, createdAt:(new Date()), updatedAt:(new Date())},
+      {city: 'DesertStan', type: 'Deluxe', price: 300, createdAt:(new Date()), updatedAt:(new Date())},
+      {city: 'DesertStan', type: 'Standard view', price: 200, createdAt:(new Date()), updatedAt:(new Date())},
+      {city: 'TundraByen', type: 'Standard twin', price: 400, createdAt:(new Date()), updatedAt:(new Date()) },
+      {city: 'TundraByen', type: 'Standard', price: 300, createdAt:(new Date()), updatedAt:(new Date())},
+      {city: 'TundraByen', type: 'Deluxe', price: 600, createdAt:(new Date()), updatedAt:(new Date())},
+      {city: 'TundraByen', type: 'Standard view', price: 500, createdAt:(new Date()), updatedAt:(new Date())},
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Hotels', null, {});
+  }
+};
