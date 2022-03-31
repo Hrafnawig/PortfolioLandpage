2a10cee4f67bd1b07fb2322c78d08a7ee926971d
Hrafnawig <kobhraf@gmail.com>
2022-03-31 21:37:52 +0300

    connecting to back

--- landpage/src/pages/AvailablePage.jsx
+++ landpage/src/pages/AvailablePage.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import axios from 'axios';
 import Moment from "moment";
 
 const Available = () => {
+    const [res,setRes] = React.useState('')
     let data = sessionStorage.getItem("availability");
     data = JSON.parse(data)
-    return (
-        <div>
-            {data.city}
-            {Moment(data.dateIn).format('DD MMM')}{Moment(data.dateOut).format('DD MMM')}
-        </div>
-    );
+    useEffect(() => {
+        axios.get('http://localhost:3000/').then((response) => {
+            setRes(response.data);
+        });
+    }, []);
+    if (!res) return null;
+        return (
+            <div>
+                {data.city}
+                {res.data[0].price}
+                {Moment(data.dateIn).format('DD MMM')}{Moment(data.dateOut).format('DD MMM')}
+            </div>
+        );
 };
 
 export default Available;
