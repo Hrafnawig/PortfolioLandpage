919008f1586c877a27fd09d1b1aceb7f0a331832
Hrafnawig <kobhraf@gmail.com>
2022-04-01 12:07:03 +0300

    db fix

--- backend/seeders/20220330162517-seed-hotels.js
+++ backend/seeders/20220330162517-seed-hotels.js
@@ -1,20 +1,116 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Hotels', [
-      {city: 'ForestStadt', type: 'Standard twin', price: 300, createdAt:(new Date()), updatedAt:(new Date())},
-      {city: 'ForestStadt', type: 'Standard', price: 200, createdAt:(new Date()), updatedAt:(new Date())},
-      {city: 'ForestStadt', type: 'Deluxe', price: 400, createdAt:(new Date()), updatedAt:(new Date())},
-      {city: 'ForestStadt', type: 'Standard view', price: 300, createdAt:(new Date()), updatedAt:(new Date())},
-      {city: 'DesertStan', type: 'Standard twin', price: 200, createdAt:(new Date()), updatedAt:(new Date()) },
-      {city: 'DesertStan', type: 'Standard', price: 100, createdAt:(new Date()), updatedAt:(new Date())},
-      {city: 'DesertStan', type: 'Deluxe', price: 300, createdAt:(new Date()), updatedAt:(new Date())},
-      {city: 'DesertStan', type: 'Standard view', price: 200, createdAt:(new Date()), updatedAt:(new Date())},
-      {city: 'TundraByen', type: 'Standard twin', price: 400, createdAt:(new Date()), updatedAt:(new Date()) },
-      {city: 'TundraByen', type: 'Standard', price: 300, createdAt:(new Date()), updatedAt:(new Date())},
-      {city: 'TundraByen', type: 'Deluxe', price: 600, createdAt:(new Date()), updatedAt:(new Date())},
-      {city: 'TundraByen', type: 'Standard view', price: 500, createdAt:(new Date()), updatedAt:(new Date())},
+      {
+        city: 'ForestStadt',
+        type: 'Standard twin',
+        price: 300,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'ForestStadt',
+        type: 'Standard',
+        price: 200,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'ForestStadt',
+        type: 'Deluxe',
+        price: 400,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'ForestStadt',
+        type: 'Standard view',
+        price: 300,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'DesertStan',
+        type: 'Standard twin',
+        price: 200,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'DesertStan',
+        type: 'Standard',
+        price: 100,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'DesertStan',
+        type: 'Deluxe',
+        price: 300,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'DesertStan',
+        type: 'Standard view',
+        price: 200,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'TundraByen',
+        type: 'Standard twin',
+        price: 400,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'TundraByen',
+        type: 'Standard',
+        price: 300,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'TundraByen',
+        type: 'Deluxe',
+        price: 600,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
+      {
+        city: 'TundraByen',
+        type: 'Standard view',
+        price: 500,
+        in: (new Date()),
+        out: (new Date()),
+        createdAt: (new Date()),
+        updatedAt: (new Date())
+      },
     ], {});
   },
 
